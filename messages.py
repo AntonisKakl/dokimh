012f02d54b12a6e417aa4c41f3220cc820046392
Antonis Kaklidakis <antoneskaklidakes@gmail.com>
2025-10-08 19:58:49 +0300

    Tests in python string variables

--- messages.py
+++ messages.py
@@ -1,3 +1,13 @@
 message = "Hello this is my first message"
 print (message)
-print(message.title)
+
+print (message.title())
+print (message.upper())
+kef = message.upper()
+print (kef)
+
+
+print (message)
+print (message.lower())
+
+
